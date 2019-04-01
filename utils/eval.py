7b7af3a630b2c4b79b9630ae1193ab35fb322360
phu-pmh <phumon91@gmail.com>
2019-04-01 11:06:09 -0400

    update eval

--- utils/eval.py
+++ utils/eval.py
@@ -1,3 +1,6 @@
+import json
+import sys
+
 def get_rb_indices(sent):
    ''' 
        sent : list of tokens in the sentence
@@ -5,10 +8,10 @@ def get_rb_indices(sent):
        returns:
 	  rb_tree : list of right branching arc indices
    ''' 
-   max_index = len(sent) - 1 
+   max_index = len(sent)  
    
-   rb_tree = [(i, max_index) for i in range(max_index)]
-   return rb_tree
+   rb_tree = [(i, max_index) for i in range(1,max_index)]
+   return set(rb_tree)
 
 
 def get_lb_indices(sent):
@@ -18,9 +21,9 @@ def get_lb_indices(sent):
        returns:
            lb_tree: list of left branching arc indices
    '''
-   length = len(sent)
-   lb_tree = [(0, i) for i in range(1, length)]
-   return lb_tree
+    length = len(sent) + 1
+    lb_tree = [(1, i) for i in range(2, length)]
+    return set(lb_tree)
 
 
 def get_undirected_arcs(tree):
@@ -38,7 +41,11 @@ def get_undirected_arcs(tree):
     return tmp_tree
 
 
-def eval(ref_tree, sys_tree):
+def mean(x):
+    return sum(x) / len(x)
+
+
+def eval(ref_trees, sys_trees, mode='sys'):
     '''
         return:
             mean F1 score
@@ -47,18 +54,44 @@ def eval(ref_tree, sys_tree):
     reca_list = []
     f1_list = []
 
-    overlap = sys_tree.intersection(ref_tree)
-    prec = float(len(overlap)) / (len(sys_tree) + 1e-8)
-    reca = float(len(overlap)) / (len(ref_tree) + 1e-8)
-    if len(sys_tree) == 0:
-        reca = 1.
-        if len(ref_tree) == 0:
-            prec = 1.
-    f1 = 2 * prec * reca / (prec + reca + 1e-8)   
+    assert len(ref_trees)==len(sys_trees), "ref and sys should have same number of sentences"
 
-    prec_list.append(prec)
-    reca_list.append(reca)
-    f1_list.append(f1) 
+    for i in ref_trees:
+        
+        ref_tree = set(tuple(x) for x in ref_trees[i]['dependencies'])
+
+        if mode!='sys':
+            ref_tree = get_undirected_arcs(ref_tree)
+        if mode=='rb':
+            sys_tree = get_rb_indices(ref_trees[i]['tokens'])
+        elif mode=='lb':
+            sys_tree = get_lb_indices(ref_trees[i]['tokens'])
+        else:
+            sys_tree = set(tuple(x) for x in sys_trees[i]['dependencies'])
+        overlap = sys_tree.intersection(ref_tree)
+        prec = float(len(overlap)) / (len(sys_tree) + 1e-8)
+        reca = float(len(overlap)) / (len(ref_tree) + 1e-8)
+
+        if len(sys_tree) == 0:
+            reca = 1.
+            if len(ref_tree) == 0:
+                prec = 1.
+        f1 = 2 * prec * reca / (prec + reca + 1e-8)   
+
+        prec_list.append(prec)
+        reca_list.append(reca)
+        f1_list.append(f1) 
 
     return mean(f1_list)
 
+if __name__ == "__main__":
+    ref_tree_path = '/scratch/sb6416/Ling3340/extract_tree/UD_English-PUD/ud_eng_pud.json'
+    sys_tree_path = '/scratch/sb6416/Ling3340/extract_tree/extracted_trees/v1/'+ sys.argv[1] #bert_large__layer00__head00.json' 
+
+    with open(ref_tree_path, 'r') as f_ref:
+        ref_trees = json.load(f_ref)
+
+    with open(sys_tree_path, 'r') as f_sys:
+        sys_trees = json.load(f_sys)
+
+    print(sys.argv[1] + '\t' + str(eval(ref_trees, sys_trees, 'lb')))
